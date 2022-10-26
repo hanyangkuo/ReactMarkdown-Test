4b77e8c8ab48230a554c33be6cb258e40e3aacfa
Young Kuo <tw01472760@gmail.com>
2022-10-26 22:11:25 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactMarkdown from 'react-markdown'
+// import addClasses from 'rehype-add-classes';
+import './style.css'
+import remarkGfm from 'remark-gfm'
+import rehypeKatex from 'rehype-katex'
+
+// const processor = rehype()
+//     .data('settings', { fragment: true })
+//     .use(addClasses, {
+//         pre: 'hljs',
+//         'h1,h2,h3': 'title',
+//         h1: 'is-1',
+//         h2: 'is-2',
+//         p: 'one two'
+//     });
+
+const markdown = `
+|Hello|Hell2|Hello3|
+|--|--|--|
+|133|1456|546102|
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{background: "red", width: "80%"}}>
+    <ReactMarkdown
+        className="HelloWorld"
+        children={markdown} 
+        remarkPlugins={[[remarkGfm, {singleTilde: true}]]}
+        // rehypePlugins={[addClasses, {table: 'testTable'}]}
+    />
     </div>
   );
 }
